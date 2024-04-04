5517e1adbf0e36a664f5cc06611924f1029498b9
Fernando Rodrigues <fernandorfigueiredotec@gmail.com>
2024-04-04 19:02:14 -0300

    :construction_worker: finish create test environment for e2e tests

--- prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -1,14 +1,38 @@
+import { PrismaClient } from "@prisma/client";
+import "dotenv/config";
+
+import { execSync } from "node:child_process";
+import { randomUUID } from "node:crypto";
 import type { Environment } from "vitest";
+
+const prisma = new PrismaClient()
+
+const generateDatabaseUrl = (schema: string): string => {
+	if (!process.env.DATABASE_URL) {
+		throw new Error("Please provide a DATABASE_URL environment variable.");
+	}
+
+	const url = new URL(process.env.DATABASE_URL);
+	url.searchParams.set("schema", schema);
+
+	return url.toString();
+};
 
 export default {
 	name: "prisma",
 	transformMode: "ssr",
 	async setup() {
-		console.log("setup");
+		const schema = randomUUID();
+		const databaseURL = generateDatabaseUrl(schema);
+
+		process.env.DATABASE_URL = databaseURL;
+
+		execSync("pnpm prisma migrate deploy")
 
 		return {
 			async teardown() {
-				console.log("teardown");
+				await prisma.$executeRawUnsafe(`DROP SCHEMA IF EXISTS "${schema}" CASCADE`)
+				await prisma.$disconnect()
 			},
 		};
 	},
